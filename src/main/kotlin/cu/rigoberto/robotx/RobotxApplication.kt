2512a37dd0c55c1e2da24d30acfb56912ced83a2
Rigoberto Perez <rigotra1984@gmail.com>
2022-06-06 13:09:05 -0400

    agregando scheduledtask

--- src/main/kotlin/cu/rigoberto/robotx/RobotxApplication.kt
+++ src/main/kotlin/cu/rigoberto/robotx/RobotxApplication.kt
@@ -2,8 +2,10 @@ package cu.rigoberto.robotx
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.scheduling.annotation.EnableScheduling
 
 @SpringBootApplication
+@EnableScheduling
 class RobotxApplication
 
 fun main(args: Array<String>) {
---
+++ src/main/kotlin/cu/rigoberto/robotx/component/ScheduledTask.kt
@@ -0,0 +1,12 @@
+package cu.rigoberto.robotx.component
+
+import org.springframework.scheduling.annotation.Scheduled
+import org.springframework.stereotype.Component
+
+@Component
+class ScheduledTask {
+    @Scheduled(fixedDelayString = "\${fixedDelay.in.miliseconds}")
+    fun checkPage() {
+        System.out.println("Estoy entrando")
+    }
+}
