cac1fd3ab6bcbfa92a40d732569e0a51a1aae263
Rigoberto Perez <rigotra1984@gmail.com>
2022-06-09 19:06:40 -0400

    encode message

--- src/main/kotlin/cu/rigoberto/robotx/util/TelegramNotifier.kt
+++ src/main/kotlin/cu/rigoberto/robotx/util/TelegramNotifier.kt
@@ -2,8 +2,11 @@ package cu.rigoberto.robotx.util
 
 import java.io.*
 import java.net.URL
+import java.net.URLEncoder
+import java.nio.charset.StandardCharsets
 import java.util.logging.Level
 import java.util.logging.Logger
+
 
 class TelegramNotifier(var apiToken: String, var chatId: String) {
     private val logger: Logger = Logger.getLogger("[TelegramNotifier]")
@@ -11,7 +14,7 @@ class TelegramNotifier(var apiToken: String, var chatId: String) {
     @Throws(Exception::class)
     fun sendToTelegram(message: String) {
         var urlString = "https://api.telegram.org/bot%s/sendMessage?chat_id=%s&text=%s"
-        urlString = java.lang.String.format(urlString, apiToken, chatId, message)
+        urlString = java.lang.String.format(urlString, apiToken, chatId, encodeValue(message))
 
         try {
             val inputStream = URL(urlString).openConnection().getInputStream()
@@ -21,6 +24,15 @@ class TelegramNotifier(var apiToken: String, var chatId: String) {
             logger.log(Level.INFO, " Send message and response: $response")
         } catch (e: IOException) {
             logger.log(Level.SEVERE, "Can not send message response!", e)
+        }
+    }
+
+    private fun encodeValue(value: String): String? {
+        return try {
+            return URLEncoder.encode(value, StandardCharsets.UTF_8.toString())
+        } catch (ex: UnsupportedEncodingException) {
+            logger.log(Level.SEVERE, "Can not encodeValue response!", ex)
+            return null
         }
     }
 }
