ab94acbc6e05d2f1e27e907dc275fdf3822ae078
Rigoberto Perez <rigotra1984@gmail.com>
2022-06-10 17:36:13 -0400

    ajustando empaquetado de react

--- src/main/kotlin/cu/rigoberto/robotx/controller/HomeController.kt
+++ src/main/kotlin/cu/rigoberto/robotx/controller/HomeController.kt
@@ -1,12 +1,12 @@
-package cu.rigoberto.robotx.controller
-
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
-
-@RestController
-@RequestMapping("/")
-class HomeController {
-    @GetMapping
-    fun index(): String = "Hello World!"
-}
+//package cu.rigoberto.robotx.controller
+//
+//import org.springframework.web.bind.annotation.GetMapping
+//import org.springframework.web.bind.annotation.RequestMapping
+//import org.springframework.web.bind.annotation.RestController
+//
+//@RestController
+//@RequestMapping("/")
+//class HomeController {
+//    @GetMapping
+//    fun index(): String = "Hello World!"
+//}
